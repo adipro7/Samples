8c951b769b7d8098299ff2b35cd2ccb32abde69c
Aditya Prakash Gupta <adityapgupta.ece18@iitbhu.ac.in>
2023-11-08 08:33:22 +0000

    8 Nov 2023

---
+++ Java/Collections/ArrayArrayList.java
@@ -0,0 +1,11 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class ArrayArrayList {
+    public void operate() {
+        int arr[] = new int[20]; // fiuxed size of 20 elements
+        
+        ArrayList<Integer> arrlist = new ArrayList<Integer>(); // initial size is 5, [`,134,354,435,3453456,45, _, _, _, _, _]
+    }
+}
